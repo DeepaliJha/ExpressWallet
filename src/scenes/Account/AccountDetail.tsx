bfbd225a812a79710a19ee308b3a3d6b1154035d
Deepali <jhadeepali2610.gmail.com>
2023-06-14 16:50:54 +0530

    body color updated

--- src/scenes/Account/AccountDetail.tsx
+++ src/scenes/Account/AccountDetail.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import { sendToken } from '../../utils/TransactionUtils';
 import { goerli } from '../../models/Chain';
 import { Account } from '../../models/Account';
@@ -8,27 +8,30 @@ import { toFixedIfNecessary } from '../../utils/AccountUtils';
 import './Account.css';
 
 interface AccountDetailProps {
-  account: Account
+  account: Account;
 }
 
-const AccountDetail: React.FC<AccountDetailProps> = ({account}) => {
+const AccountDetail: React.FC<AccountDetailProps> = ({ account }) => {
   const [destinationAddress, setDestinationAddress] = useState('');
   const [amount, setAmount] = useState(0);
-  const [balance, setBalance] = useState(account.balance)
+  const [balance, setBalance] = useState(account.balance);
 
-  const [networkResponse, setNetworkResponse] = useState<{ status: null | 'pending' | 'complete' | 'error', message: string | React.ReactElement }>({
+  const [networkResponse, setNetworkResponse] = useState<{
+    status: null | 'pending' | 'complete' | 'error';
+    message: string | React.ReactElement;
+  }>({
     status: null,
     message: '',
   });
 
   useEffect(() => {
     const fetchData = async () => {
-        const provider = new ethers.providers.JsonRpcProvider(goerli.rpcUrl);
-        let accountBalance = await provider.getBalance(account.address);
-        setBalance((String(toFixedIfNecessary(ethers.utils.formatEther(accountBalance)))));
-    }
+      const provider = new ethers.providers.JsonRpcProvider(goerli.rpcUrl);
+      let accountBalance = await provider.getBalance(account.address);
+      setBalance(String(toFixedIfNecessary(ethers.utils.formatEther(accountBalance))));
+    };
     fetchData();
-}, [account.address])
+  }, [account.address]);
 
   function handleDestinationAddressChange(event: React.ChangeEvent<HTMLInputElement>) {
     setDestinationAddress(event.target.value);
@@ -52,9 +55,14 @@ const AccountDetail: React.FC<AccountDetailProps> = ({account}) => {
         // Set the network response status to "complete" and the message to the transaction hash
         setNetworkResponse({
           status: 'complete',
-          message: <p>Transfer complete! <a href={`${goerli.blockExplorerUrl}/tx/${receipt.transactionHash}`} target="_blank" rel="noreferrer">
-            View transaction
-            </a></p>,
+          message: (
+            <p>
+              Transfer complete!{' '}
+              <a href={`${goerli.blockExplorerUrl}/tx/${receipt.transactionHash}`} target="_blank" rel="noreferrer">
+                View transaction
+              </a>
+            </p>
+          ),
         });
         return receipt;
       } else {
@@ -79,55 +87,51 @@ const AccountDetail: React.FC<AccountDetailProps> = ({account}) => {
   }
 
   return (
-    <div className='AccountDetail container'>
-        <h4>
-            Address: <a href={`https://goerli.etherscan.io/address/${account.address}`} target="_blank" rel="noreferrer">
-            {account.address}
-            </a><br/>
-            Balance: {balance} ETH
-        </h4>
+    <div className="AccountDetail container" >
+      <h4>
+        Address:{' '}
+        <a href={`https://goerli.etherscan.io/address/${account.address}`} target="_blank" rel="noreferrer">
+          {account.address}
+        </a>
+        <br />
+        Balance: {balance} ETH
+      </h4>
 
-        <div className="form-group">
-            <label>Destination Address:</label>
-            <input
-            className="form-control"
-            type="text"
-            value={destinationAddress}
-            onChange={handleDestinationAddressChange}
-            />
-        </div>
+      <div className="form-group">
+        <label>Destination Address:</label>
+        <input
+          className="form-control"
+          type="text"
+          value={destinationAddress}
+          onChange={handleDestinationAddressChange}
+        />
+      </div>
 
-        <div className="form-group">
-            <label>Amount:</label>
-            <input
-            className="form-control"
-            type="number"
-            value={amount}
-            onChange={handleAmountChange}
-            />
-        </div>
+      <div className="form-group">
+        <label>Amount:</label>
+        <input className="form-control" type="number" value={amount} onChange={handleAmountChange} />
+      </div>
 
-        <button
-            className=""
-            type="button"
-            onClick={transfer}
-            disabled={!amount || networkResponse.status === 'pending'}
-        >
-            Send {amount} ETH
-        </button>
+      <button
+        className="btn btn-primary"
+        type="button"
+        onClick={transfer}
+        disabled={!amount || networkResponse.status === 'pending'}
+      >
+        Send {amount} ETH
+      </button>
 
-        {networkResponse.status &&
-            <>
-            {networkResponse.status === 'pending' && <p>Transfer is pending...</p>}
-            {networkResponse.status === 'complete' && <p>{networkResponse.message}</p>}
-            {networkResponse.status === 'error' && <p>Error occurred while transferring tokens: {networkResponse.message}</p>}
-            </>
-        }
+      {networkResponse.status && (
+        <>
+          {networkResponse.status === 'pending' && <p>Transfer is pending...</p>}
+          {networkResponse.status === 'complete' && <p>{networkResponse.message}</p>}
+          {networkResponse.status === 'error' && <p>Error occurred while transferring tokens: {networkResponse.message}</p>}
+        </>
+      )}
 
-        <AccountTransactions account={account} />
+      <AccountTransactions account={account} />
     </div>
-
-  )
-}
+  );
+};
 
 export default AccountDetail;
